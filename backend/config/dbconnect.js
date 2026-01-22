2d55e392a8eee9e4e56aaaeadebac41e685296df
shahfawadkhan <sfawadkhan92@gmail.com>
2026-01-22 23:37:24 +0500

    connect db

--- backend/config/dbconnect.js
+++ backend/config/dbconnect.js
@@ -1,6 +1,5 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
+import mongoose from "mongoose";
+export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
@@ -11,4 +10,3 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/dbconnect.js";
+import {connectDB} from './config/dbconnect.js'
 
 dotenv.config();
 
