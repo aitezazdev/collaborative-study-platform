e38863ab5f0ae8d72708dde8b9afcad1bf6ea8be
shahfawadkhan <sfawadkhan92@gmail.com>
2026-01-23 22:29:03 +0500

    date issue fixed

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -25,6 +25,7 @@ export const login = async (req, res) => {
         id: user._id,
         name: user.name,
         email: user.email,
+        joined: user.createdAt
       },
     });
   } catch (error) {
